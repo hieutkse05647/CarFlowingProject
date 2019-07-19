ca69ab69d8d1ae7ec175a646e49b719f43e4ed0c
Từ Khắc Hiếu <hieutk.fpt@gmail.com>
2019-07-20 00:42:35 +0700

    push to branch
    all changed

--- app/src/main/java/com/ebookfrenzy/carflowingproject/MainActivity.java
+++ app/src/main/java/com/ebookfrenzy/carflowingproject/MainActivity.java
@@ -1,13 +1,68 @@
 package com.ebookfrenzy.carflowingproject;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+    private ActionBar toolbar;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment fragment;
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    toolbar.setTitle("Home");
+                    return true;
+                case R.id.navigation_activity:
+//                    Intent intent = new Intent(this, ActiveCar.class);
+//                    startActivity(intent);
+                    return true;
+                case R.id.navigation_cars:
+                    toolbar.setTitle("Cars");
+                    return true;
+                case R.id.navigation_alarm:
+                    toolbar.setTitle("Alarm");
+                    return true;
+                case R.id.navigation_user:
+                    toolbar.setTitle("User");
+                    return true;
+            }
+            return false;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        toolbar = getSupportActionBar();
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        toolbar.setTitle("Following Car Project");
+    }
+
+    public void ActiveCarActivity (View view){
+        Intent intent = new Intent(this, ActiveCar.class );
+        startActivity(intent);
+    }
+
+    public void TrackingGPSActivity (View view){
+        Intent intent = new Intent(this, GPSActivity.class );
+        startActivity(intent);
     }
 }
